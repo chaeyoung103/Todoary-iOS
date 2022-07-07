f7f0bc949bd4122c605f88deb090806b458cf676
Ssooya <bsy24660@naver.com>
2022-07-07 16:07:42 +0900

    [fix]:remove_setup_line_height

--- Todoary-iOS/Todoary/CommonSource/Extensions/UILabel.swift
+++ Todoary-iOS/Todoary/CommonSource/Extensions/UILabel.swift
@@ -114,23 +114,7 @@ extension UILabel {
             self.attributedText = attrString
         }
     }
-    
-    func setTextWithLineHeight(text: String?, lineHeight: CGFloat){
-        if let text = text {
-            let style = NSMutableParagraphStyle()
-            style.maximumLineHeight = lineHeight
-            style.minimumLineHeight = lineHeight
 
-            let attributes: [NSAttributedString.Key: Any] = [
-                .paragraphStyle: style,
-                .baselineOffset: (lineHeight - font.lineHeight) / 4
-            ]
-
-            let attrString = NSAttributedString(string: text,
-                                                attributes: attributes)
-            self.attributedText = attrString
-        }
-    }
 }
 
 
