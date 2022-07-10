77df6420a386908d6a8e0bf87433e58e0b40dfb1
Ssooya <bsy24660@naver.com>
2022-07-10 16:36:33 +0900

    [fix]:uifont_nbfont_combination_code_error_fix

--- Todoary-iOS/Todoary/Setting/AlarmSettingViewController.swift
+++ Todoary-iOS/Todoary/Setting/AlarmSettingViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class AlarmSettingViewController: UIViewController {
+    
+    var navigationView : NavigationView!
+    
+    var tableView : UITableView!
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationView = NavigationView(frame: .zero, self.navigationController!).then{
+            $0.navigationTitle.text = "알림"
+        }
+        
+        tableView = UITableView().then{
+            $0.separatorStyle = .none
+            $0.register(AlarmSettingTableViewCell.self, forCellReuseIdentifier: "alarmSettingCell")
+            $0.delegate = self
+            $0.dataSource = self
+        }
+        
+        setUpView()
+        setUpConstraint()
+        
     }
+}
+
+extension AlarmSettingViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return AlarmSettingTableViewCell()
+    }
+    
+    
+    
 }
