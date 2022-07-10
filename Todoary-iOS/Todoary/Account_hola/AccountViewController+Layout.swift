a58f0af0dce813099520e3baf24eaac6f508b30d
chaeyoung103 <thdcodud103@naver.com>
2022-07-10 18:40:46 +0900

    [feat]: add_accountviewcontroller

---
+++ Todoary-iOS/Todoary/Account_hola/AccountViewController+Layout.swift
@@ -0,0 +1,67 @@
+//
+//  AccountViewController+Layout.swift
+//  Todoary
+//
+//  Created by 송채영 on 2022/07/10.
+//
+
+import UIKit
+
+extension AccountViewController {
+    
+    func setUpView(){
+        
+        self.view.addSubview(contentView)
+        
+        self.view.addSubview(navigationView)
+        
+        self.view.addSubview(profileImage)
+        self.view.addSubview(nickName)
+        self.view.addSubview(introduce)
+        self.view.addSubview(profileChangeBtn)
+
+
+    
+    }
+    
+    func setUpConstraint(){
+        
+        //background
+        contentView.snp.makeConstraints{ make in
+            make.top.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview()
+        }
+        
+        //navigation bar
+        navigationView.snp.makeConstraints{ make in
+            make.top.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+
+        //profile
+        profileImage.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(135)
+            make.leading.equalToSuperview().offset(42)
+            make.width.equalTo(85)
+            make.height.equalTo(85)
+        }
+        
+        nickName.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(155)
+            make.leading.equalTo(profileImage.snp.trailing).offset(20)
+        }
+        
+        introduce.snp.makeConstraints{ make in
+            make.top.equalTo(nickName.snp.bottom).offset(10)
+            make.leading.equalTo(nickName.snp.leading)
+        }
+        
+        profileChangeBtn.snp.makeConstraints{ make in
+            make.bottom.equalToSuperview().offset(-33.67)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(171)
+            make.height.equalTo(25)
+        }
+    }
+}
