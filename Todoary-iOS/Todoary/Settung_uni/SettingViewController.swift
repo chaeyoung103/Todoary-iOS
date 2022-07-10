14680910bcc14ec181ac0aed9a4f7868bca2aba2
yooyeri <dbdpfl0105@naver.com>
2022-07-11 01:12:39 +0900

    [feat] : tableView

--- Todoary-iOS/Todoary/Settung_uni/SettingViewController.swift
+++ Todoary-iOS/Todoary/Settung_uni/SettingViewController.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class SettingViewController : UIViewController {
+    
+    //MARK: - UIComponenets
+
+    let contentView = UIView().then{
+        $0.backgroundColor = .white
+    }
+    
+    //navigation bar
+    var navigationView : NavigationView!
+    
+    //tableView
+    var tableView : UITableView!
+    
+    //t
+    
+    //MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationView = NavigationView(frame: .zero , self.navigationController!).then{
+            $0.navigationTitle.text = "설정"
+        }
+        
+        tableView = UITableView().then{
+            $0.separatorStyle = .none
+            $0.register(SettingTableViewCell.self, forCellReuseIdentifier: "SettingTableViewCell")
+            $0.delegate = self
+            $0.dataSource = self
+        }
+
+        setUpView()
+        setUpConstraint()
+    }
+    
+}
+
+
+//MARK: - Helpers
+
+extension AccountViewController: UITableViewDelegate, UITableViewDataSource{
+
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "accountTableViewCell", for: indexPath) as? AccountTableViewCell else{
+        return UITableViewCell()
+    }
+    
+    switch indexPath.row{
+    case 0:
+        cell.title.text = "비밀번호 재설정"
+        return cell
+    case 1:
+        cell.title.text = "로그아웃"
+        cell.nextBtn.isHidden = true
+        return cell
+    case 2:
+        cell.title.text = "계정 삭제하기"
+        cell.title.textColor = .deleteRed
+        cell.nextBtn.isHidden = true
+        return cell
+    default:
+        fatalError("TableViewCell Error")
+    }
+}
+
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("select \(indexPath.row)")
+    }
+}
