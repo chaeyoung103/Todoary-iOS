757f49d289436f14ccdfe3f0dd5e9bdb3e4b5270
Ssooya <bsy24660@naver.com>
2022-07-12 15:18:04 +0900

    [feat]:alarmVC_popup_design_edit

--- Todoary-iOS/Todoary/Setting/InfoMessageView.swift
+++ Todoary-iOS/Todoary/Setting/InfoMessageView.swift
@@ -9,73 +9,28 @@ import UIKit
 
 class InfoMessageView: UIView {
     
+    let silverView = UIView().then{
+        $0.backgroundColor = UIColor(red: 196/255, green: 196/255, blue: 196/255, alpha: 1)
+        $0.layer.cornerRadius = 10
+        $0.layer.shadowRadius = 5.0
+        $0.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+        $0.layer.shadowOffset = CGSize(width: 0, height: 1)
+        $0.layer.shadowOpacity = 0.65
+        $0.layer.masksToBounds = false
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
-
-        var view = UILabel()
-
-        view.frame = CGRect(x: 0, y: 0, width: 137, height: 53)
-
-        view.backgroundColor = .white
-
-
-        var shadows = UIView()
-
-        shadows.frame = view.frame
-
-        shadows.clipsToBounds = false
-
-        view.addSubview(shadows)
-
-
-        let shadowPath0 = UIBezierPath(roundedRect: shadows.bounds, cornerRadius: 10)
-
-        let layer0 = CALayer()
-
-        layer0.shadowPath = shadowPath0.cgPath
-
-        layer0.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.45).cgColor
-
-        layer0.shadowOpacity = 1
-
-        layer0.shadowRadius = 5
-
-        layer0.shadowOffset = CGSize(width: 0, height: 1)
-
-        layer0.bounds = shadows.bounds
-
-        layer0.position = shadows.center
-
-        shadows.layer.addSublayer(layer0)
-
-
-        var shapes = UIView()
-
-        shapes.frame = view.frame
-
-        shapes.clipsToBounds = true
-
-        view.addSubview(shapes)
-
-
-        let layer1 = CALayer()
-
-        layer1.backgroundColor = UIColor(red: 0.769, green: 0.769, blue: 0.769, alpha: 1).cgColor
-
-        layer1.bounds = shapes.bounds
-
-        layer1.position = shapes.center
-
-        shapes.layer.addSublayer(layer1)
-
-
-        shapes.layer.cornerRadius = 10
         
-        self.addSubview(view)
+        self.addSubview(silverView)
         
-        view.snp.makeConstraints{ make in
+        self.snp.makeConstraints{ make in
             make.width.equalTo(137)
             make.height.equalTo(53)
+        }
+        
+        silverView.snp.makeConstraints{ make in
+            make.leading.trailing.top.bottom.equalToSuperview()
         }
     }
     
