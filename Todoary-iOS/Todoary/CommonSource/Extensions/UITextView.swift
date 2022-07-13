a2b400bb058d1ae96c6ecd50eefc2107c954261f
Ssooya <bsy24660@naver.com>
2022-07-13 16:07:11 +0900

    [feat]:add_textview_auto

---
+++ Todoary-iOS/Todoary/CommonSource/Extensions/UITextView.swift
@@ -0,0 +1,62 @@
+//
+//  UITextView.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/07/13.
+//
+
+import UIKit
+
+extension UITextView {
+    
+    
+    //button type별 자간, 폰트 자동화 함수
+    func textViewTypeSetting(){
+
+        if let labelText = text, labelText.isEmpty == false {
+            
+            var attributedString = NSMutableAttributedString(string: labelText)
+            
+            attributedString = addLetterSpacing(attributedString: attributedString)
+            attributedString = setTextWithLineHeight(attributedString: attributedString)
+
+            attributedText = attributedString
+        }
+        
+        font = UIFont.nbFont(ofSize: 16, weight: .medium)
+        textColor = .black
+        
+    }
+    
+    private func addLetterSpacing(attributedString: NSMutableAttributedString) -> NSMutableAttributedString{
+        
+        attributedString.addAttribute(.kern,
+                                      value: 0.32,
+                                      range: NSRange(location: 0,
+                                                     length: attributedString.length-1))
+        
+        return attributedString
+    }
+    
+    private func setTextWithLineHeight(attributedString: NSMutableAttributedString) -> NSMutableAttributedString{
+        
+        let lineHeight: Double = 19.2
+        
+        let style = NSMutableParagraphStyle()
+        style.maximumLineHeight = lineHeight
+        style.minimumLineHeight = lineHeight
+        
+        style.lineSpacing = lineHeight
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .paragraphStyle: style,
+//            .baselineOffset: (lineHeight - font.lineHeight) / 4
+        ]
+        
+        attributedString.addAttributes(attributes,
+                                       range: NSRange(location: 0,
+                                                      length: attributedString.length-1))
+        return attributedString
+    }
+
+}
