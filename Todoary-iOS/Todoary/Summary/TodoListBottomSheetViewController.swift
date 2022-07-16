3d0410a3944c7ad60bedca19c5466f6cefac95bd
Ssooya <bsy24660@naver.com>
2022-07-16 14:49:37 +0900

    [feat]:3rd_week_uicolor_add

--- Todoary-iOS/Todoary/Summary/TodoListBottomSheetViewController.swift
+++ Todoary-iOS/Todoary/Summary/TodoListBottomSheetViewController.swift
@@ -8,11 +8,70 @@
 import UIKit
 
 class TodoListBottomSheetViewController: UIViewController {
+    
+    let todoListTitle = UILabel().then{
+        $0.text = "TODO LIST"
+    }
+    
+    var tableView : UITableView!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(red: 134/255, green: 182/255, blue: 255/255, alpha: 1)
 
-        // Do any additional setup after loading the view.
+        tableView = UITableView().then{
+            $0.delegate = self
+            $0.dataSource = self
+            
+            $0.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+            
+            $0.separatorStyle = .none
+            
+            $0.register(TodoListTableViewCell.self, forCellReuseIdentifier: TodoListTableViewCell.cellIdentifier)
+        }
+        
+        setUpView()
+        
+        setUpConstraint()
     }
+    
+    func setUpView(){
+        self.view.addSubview(todoListTitle)
+        self.view.addSubview(tableView)
+    }
+    
+    func setUpConstraint(){
+        
+        todoListTitle.snp.makeConstraints{ make in
+            make.leading.equalToSuperview().offset(32)
+            make.top.equalToSuperview().offset(33)
+        }
+        
+        tableView.snp.makeConstraints{ make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(todoListTitle.snp.bottom).offset(16)
+            make.bottom.equalToSuperview()
+        }
+    }
+
+}
+
+extension TodoListBottomSheetViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: TodoListTableViewCell.cellIdentifier, for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
 
 }
--- Todoary-iOS/Todoary/Summary/TodoListTableViewCell.swift
+++ Todoary-iOS/Todoary/Summary/TodoListTableViewCell.swift
@@ -6,11 +6,63 @@
 //
 
 import UIKit
+import SnapKit
 
 class TodoListTableViewCell: UITableViewCell {
+    
+    static let cellIdentifier = "todoListCell"
+    
+    let selectedBackView = UIView().then{
+        $0.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+    }
+    
+    let checkBox = UIButton().then{
+        $0.setImage(UIImage(named: "check_box_outline_blank"), for: .normal)
+        $0.setImage(UIImage(named: "check_box"), for: .selected)
+    }
+    
+    let titleLabel = UILabel().then{
+        $0.text = "아침 산책"
+        $0.textColor = .black
+        $0.font = UIFont.nbFont(ofSize: 15, weight: .bold)
+        $0.addLetterSpacing(spacing: 0.3)
+    }
+    
+    let categoryButton = UIButton().then{
+        $0.setTitle("운동", for: .normal)
+        $0.titleLabel?.font = UIFont.nbFont(ofSize: 12, weight: .bold)
+        $0.addLetterSpacing(spacing: 0.24)
+    }
+    
+    let pinImage = UIImageView().then{
+        $0.image = UIImage(named: "document")
+    }
+    
+    let alarmImage = UIImageView().then{
+        $0.image = UIImage(named: "document")
+    }
+    
+    let timeLabel = UILabel().then{
+        $0.text = "AM 7:00"
+        $0.font = UIFont.nbFont(ofSize: 13, weight: .medium)
+        $0.addLetterSpacing(spacing: -0.26)
+        $0.textAlignment = .right
+    }
+    
+    let stackView = UIStackView().then{
+        $0.axis = .horizontal
+        $0.spacing = 2
+    }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        
+        self.contentView.backgroundColor = .white
+        self.contentView.layer.cornerRadius = 20
+        
         
         setUpView()
         setUpConstraint()
@@ -22,9 +74,87 @@ class TodoListTableViewCell: UITableViewCell {
     
     func setUpView(){
         
+        stackView.addArrangedSubview(pinImage)
+        stackView.addArrangedSubview(alarmImage)
+        stackView.addArrangedSubview(timeLabel)
+        
+        self.contentView.addSubview(checkBox)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(categoryButton)
+        self.contentView.addSubview(stackView)
+        
+        self.selectedBackgroundView = selectedBackView
+        
+        
+        self.backgroundColor = .red
+        self.contentView.backgroundColor = .green
+        
     }
     
     func setUpConstraint(){
         
+//        self.snp.makeConstraints{ make in
+////            make.leading.equalToSuperview().offset(32)
+////            make.trailing.equalToSuperview().offset(-30)
+//            make.width.equalTo(328)
+//            make.height.equalTo(60)
+//        }
+        
+        self.contentView.snp.makeConstraints{ make in
+            make.leading.equalToSuperview().offset(32)
+            make.trailing.equalToSuperview().offset(-30)
+            make.height.equalTo(60)
+            make.centerY.equalToSuperview()
+        }
+        
+        checkBox.snp.makeConstraints{ make in
+            make.width.height.equalTo(24)
+            make.leading.equalToSuperview().offset(19)
+            make.centerY.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints{ make in
+            make.leading.equalTo(checkBox.snp.trailing).offset(13)
+            make.top.equalToSuperview().offset(20.09)
+            make.bottom.equalToSuperview().offset(-23.89)
+        }
+        
+        categoryButton.snp.makeConstraints{ make in
+            make.width.equalTo(46)
+            make.height.equalTo(21)
+            make.trailing.equalTo(stackView.snp.leading).offset(-7)
+            make.top.equalToSuperview().offset(21)
+            make.bottom.equalToSuperview().offset(-18)
+        }
+        
+        
+        pinImage.snp.makeConstraints{ make in
+            make.width.equalTo(14)
+            make.height.equalTo(13.2)
+        }
+        
+        
+        alarmImage.snp.makeConstraints{ make in
+            make.width.equalTo(14)
+            make.height.equalTo(13.2)
+        }
+        
+        
+        timeLabel.snp.makeConstraints{ make in
+            
+        }
+        
+        
+        stackView.snp.makeConstraints{ make in
+            make.trailing.equalToSuperview().offset(-18)
+            make.top.equalTo(23.4)
+            make.bottom.equalTo(-22.46)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 15, right: 0))
     }
 }
