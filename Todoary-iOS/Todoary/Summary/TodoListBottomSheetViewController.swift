fd6e7ea871c6da4ab517d4382d91bf8394e36d9d
Ssooya <bsy24660@naver.com>
2022-07-16 12:58:03 +0900

    [feat]:todolistVC_file_add

---
+++ Todoary-iOS/Todoary/Summary/TodoListBottomSheetViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListBottomSheetViewController.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/07/16.
+//
+
+import UIKit
+
+class TodoListBottomSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Todoary-iOS/Todoary/Summary/TodoListTableViewCell.swift
+++ Todoary-iOS/Todoary/Summary/TodoListTableViewCell.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class TodoListTableViewCell: UITableViewCell {
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUpView()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpView(){
+        
+    }
+    
+    func setUpConstraint(){
+        
+    }
 }
