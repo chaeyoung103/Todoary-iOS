ed1e859b216c354421b837d6f3ea8f537bc532dd
Ssooya <bsy24660@naver.com>
2022-07-17 17:31:12 +0900

    [feat]:extra_bold_font_Add

--- Todoary-iOS/Todoary/Summary/TodoListBottomSheetViewController.swift
+++ Todoary-iOS/Todoary/Summary/TodoListBottomSheetViewController.swift
@@ -11,6 +11,7 @@ class TodoListBottomSheetViewController: UIViewController {
     
     let todoListTitle = UILabel().then{
         $0.text = "TODO LIST"
+        $0.font = UIFont.nbFont(ofSize: 15, weight: .extraBold)
     }
     
     var tableView : UITableView!
@@ -18,6 +19,12 @@ class TodoListBottomSheetViewController: UIViewController {
     let hiddenView = HiddenSettingView()
 
     override func viewDidLoad() {
+        
+        for fontFamily in UIFont.familyNames {
+            for fontName in UIFont.fontNames(forFamilyName: fontFamily) {
+                print(fontName)
+            }
+        }
         
         super.viewDidLoad()
         
@@ -40,17 +47,9 @@ class TodoListBottomSheetViewController: UIViewController {
     func setUpView(){
         self.view.addSubview(todoListTitle)
         self.view.addSubview(tableView)
-        
-        self.view.addSubview(hiddenView)
-        self.view.sendSubviewToBack(hiddenView)
     }
     
     func setUpConstraint(){
-        
-        hiddenView.snp.makeConstraints{ make in
-            make.leading.equalToSuperview().offset(32)
-            make.trailing.equalToSuperview().offset(-30)
-        }
         
         todoListTitle.snp.makeConstraints{ make in
             make.leading.equalToSuperview().offset(32)
@@ -78,81 +77,5 @@ extension TodoListBottomSheetViewController: UITableViewDelegate, UITableViewDat
         
         return cell
     }
-    
-    //leading
-    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let pin = UIContextualAction(style: .normal, title: nil) { (UIContextualAction, UIView, success: @escaping (Bool) -> Void) in
-            print("Share 클릭 됨")
-            success(true)
-        }
-
-        pin.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
-        pin.image = UIImage(named: "push_pin_big")
-//        delete.backgroundColor = .white
-
-        return UISwipeActionsConfiguration(actions: [pin])
-    }
-
-    //trailing
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
-        guard let cell = tableView.cellForRow(at: indexPath) as? TodoListTableViewCell else{
-            return nil
-        }
-        
-//        self.view.addSubview(hiddenView)
-//        self.view.sendSubviewToBack(hiddenView)
-        
-        hiddenView.snp.makeConstraints{ make in
-//            make.leading.equalToSuperview().offset(32)
-//            make.trailing.equalToSuperview().offset(-30)
-            make.top.equalTo(cell.contentView)
-            make.bottom.equalTo(cell.contentView)
-        }
-        
-        hiddenView.updateConstraints()
-
-        let setting = UIContextualAction(style: .normal, title: nil) { (UIContextualAction, UIView, success: @escaping (Bool) -> Void) in
-            UIView.layer.cornerRadius = 10
-            print("setting 클릭 됨")
-            success(true)
-        }
-
-        setting.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
-//        setting.image = UIImage(named: "settings")
-
-
-        let delete = UIContextualAction(style: .normal, title: nil) { (UIContextualAction, UIView, success: @escaping (Bool) -> Void) in
-            print("delete 클릭 됨")
-            success(true)
-        }
-
-        delete.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
-//        delete.image = UIImage(named: "trash")
-
-        return UISwipeActionsConfiguration(actions: [delete, setting])
-    }
-
-
-    
-    //trailing
-//    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-//
-//
-//        return UISwipeActionsConfiguration()
-//    }
-    
-//    func hiddenSettingViewShow(_ contentView: UIContentView){
-//
-//        self.view.addSubview(hiddenView)
-//        self.view.sendSubviewToBack(hiddenView)
-//
-//        hiddenView.snp.makeConstraints{ make in
-//            make.leading.equalToSuperview().offset(32)
-//            make.trailing.equalToSuperview().offset(-30)
-//            make.top.equalTo(contentView)
-//            make.bottom.equalTo(contentView)
-//        }
-//    }
     
 }
