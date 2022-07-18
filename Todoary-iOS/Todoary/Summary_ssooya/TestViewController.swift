5f818f59e750f654e883a8d17a8f75901b249b57
Ssooya <bsy24660@naver.com>
2022-07-18 16:18:42 +0900

    [feat]:Add_file_for_sheetVC_height_custom

--- Todoary-iOS/Todoary/Summary_ssooya/TestViewController.swift
+++ Todoary-iOS/Todoary/Summary_ssooya/TestViewController.swift
@@ -18,13 +18,15 @@ class TestViewController: UIViewController {
     func show(){
         
         let viewControllerToPresent = TodoListBottomSheetViewController()
-        if let sheet = viewControllerToPresent.sheetPresentationController {
-            sheet.detents = [.medium(), .large()]
-            sheet.largestUndimmedDetentIdentifier = .medium
-            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
-            sheet.prefersEdgeAttachedInCompactHeight = true
-            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
-        }
+        
+//        if let sheet = viewControllerToPresent.sheetPresentationController {
+//            sheet.detents = [.medium(),.large()]
+//            sheet.largestUndimmedDetentIdentifier = .medium
+//            sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+//            sheet.prefersEdgeAttachedInCompactHeight = true
+//            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
+//        }
+        viewControllerToPresent.loadViewIfNeeded()
         present(viewControllerToPresent, animated: true, completion: nil)
     }
 
