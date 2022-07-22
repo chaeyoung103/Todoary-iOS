7c8895fb8168612d032172171fc6d6865a82a327
yooyeri <dbdpfl0105@naver.com>
2022-07-22 14:24:53 +0900

    [feat] : add_DataManager

---
+++ Todoary-iOS/Todoary/2week/Profile_hola/GetProfileDataManager/GetProfileInput.swift
@@ -0,0 +1,8 @@
+//
+//  GetProfileInput.swift
+//  Todoary
+//
+//  Created by 예리 on 2022/07/22.
+//
+
+import Foundation
--- Todoary-iOS/Todoary/Login_hola/AutoLoginDataManager/AutoLoginDataManager.swift
+++ Todoary-iOS/Todoary/Login_hola/AutoLoginDataManager/AutoLoginDataManager.swift
@@ -5,4 +5,27 @@
 //  Created by 박지윤 on 2022/07/22.
 //
 
-import Foundation
+import Alamofire
+
+class AutoLoginDataManager {
+    func auto(_ veiwController : LoginViewController,
+              _ parameter : AutoLoginInput) {
+        
+        //통신
+        AF.request("http://todoary.com:9000/auth/signin/auto",
+                   method: .post, parameters: parameter,
+                   encoder: JSONParameterEncoder.default, headers: nil)
+            .validate()
+            .responseDecodable(of: AutoLoginModel.self) {response in
+            switch response.result {
+            case .success(let result) :
+                print("DEBUG: ", result)
+
+            case .failure(let error) :
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+    
+
