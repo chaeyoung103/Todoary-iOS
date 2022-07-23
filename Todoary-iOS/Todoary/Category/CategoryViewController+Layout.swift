41a1018497b0a9595a0c3c4c5e0ea100750641a9
Ssooya <bsy24660@naver.com>
2022-07-23 17:37:39 +0900

    [feat]:categoryVC layout 파일 추가

---
+++ Todoary-iOS/Todoary/Category/CategoryViewController+Layout.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryViewController+Layout.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/07/23.
+//
+
+import Foundation
+
+extension CategoryViewController{
+    
+    func setUpView(){
+        
+    }
+    
+    func setUpConstraint(){
+        
+    }
+}
--- Todoary-iOS/Todoary/Category/CategoryViewController.swift
+++ Todoary-iOS/Todoary/Category/CategoryViewController.swift
@@ -9,25 +9,27 @@ import UIKit
 
 class CategoryViewController: UIViewController {
     
-//    let navigationView : NavigationView!
-////        .then{
-////    }
-//    
-//    let trashButton = UIButton()
-////        .then{
-////    }
-//    
-//    let collectionView : UICollectionView!
-//    
-//    let tableView : UITableView!
-//    
-//    let todoAddBtn = UIButton()
-////        .then{
-////
-////    }
+    var navigationView : NavigationView!
+
+    let trashButton = UIButton()
+//        .then{
+//    }
+
+    var collectionView : UICollectionView!
+
+    var tableView : UITableView!
+
+    let todoAddBtn = UIButton()
+//        .then{
+//
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        navigationView = NavigationView(frame: .zero, self.navigationController!)
 
     }
     
