01fa4a33f59b033dc9d83a21af3465fd1fbc4d42
Ssooya <bsy24660@naver.com>
2022-07-23 17:30:43 +0900

    [style]:change_ 파일 이름

---
+++ Todoary-iOS/Todoary/Category/CategoryButtonCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryButton.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/07/23.
+//
+
+import UIKit
+
+class CategoryButtonCollectionViewCell: UICollectionViewCell {
+
+}
--- Todoary-iOS/Todoary/Category/CategoryViewController.swift
+++ Todoary-iOS/Todoary/Category/CategoryViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
+    
+//    let navigationView : NavigationView!
+////        .then{
+////    }
+//    
+//    let trashButton = UIButton()
+////        .then{
+////    }
+//    
+//    let collectionView : UICollectionView!
+//    
+//    let tableView : UITableView!
+//    
+//    let todoAddBtn = UIButton()
+////        .then{
+////
+////    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
