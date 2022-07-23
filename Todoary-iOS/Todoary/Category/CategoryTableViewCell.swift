297b08159e143aef8eb2cc52146bd3371c95ccc4
Ssooya <bsy24660@naver.com>
2022-07-23 17:07:44 +0900

    [feat]:Add_카테고리 파일 추가

---
+++ Todoary-iOS/Todoary/Category/CategoryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryTableViewCell.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/07/23.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
