e8df8977fdd27d5237f6fe023adbf1e8e9c0b5c3
Ssooya <bsy24660@naver.com>
2022-07-27 11:41:22 +0900

    [feat]:Add_DatabaseAlert 추가 및 SignUp API 변경사항 반영

--- Todoary-iOS/Todoary/CommonSource/DataBaseErrorAlert.swift
+++ Todoary-iOS/Todoary/CommonSource/DataBaseErrorAlert.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class DataBaseErrorAlert: UIAlertController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        self.title = "서버 오류"
+        self.message = "서버에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요."
+        
+        let alertAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        self.addAction(alertAction)
+        
     }
-    */
 
 }
