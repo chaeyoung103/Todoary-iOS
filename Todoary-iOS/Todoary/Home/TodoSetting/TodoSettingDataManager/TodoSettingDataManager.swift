ad31505bb992b0a6c33e78278e8f4549107682a2
chaeyoung103 <thdcodud103@naver.com>
2022-07-27 20:59:44 +0900

    [feat]: todoapi 생성

---
+++ Todoary-iOS/Todoary/Home/TodoSetting/TodoSettingDataManager/TodoSettingDataManager.swift
@@ -0,0 +1,33 @@
+//
+//  TodoSettingDataManager.swift
+//  Todoary
+//
+//  Created by 송채영 on 2022/07/27.
+//
+
+import Alamofire
+
+class TodoSettingDataManager {
+    
+    let headers : HTTPHeaders = [.authorization(UserDefaults.standard.string(forKey: "accessToken")!)]
+    
+    func todoSettingDataManager( _ viewController : TodoSettingViewController , _ parameter: ProfileInput) {
+        AF.request("https://todoary.com/todo", method: .post, parameters: parameter,  encoder: JSONParameterEncoder.default , headers: headers).validate().responseDecodable(of: ProfileModel.self) { response in
+            switch response.result {
+            case .success(let result):
+                switch result.code {
+                case 1000:
+                    print("프로필수정성공")
+                case 2201:
+                    print("중복된 닉네임입니다")
+                default:
+                    print(result.message)
+                }
+
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
