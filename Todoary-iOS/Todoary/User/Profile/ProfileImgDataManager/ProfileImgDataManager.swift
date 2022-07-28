5bb9b4f5842f5fe9ec3c40593495ba3e68087203
yooyeri <dbdpfl0105@naver.com>
2022-07-28 21:41:24 +0900

    [fiix] : pull

--- Todoary-iOS/Todoary/User/Profile/ProfileImgDataManager/ProfileImgDataManager.swift
+++ Todoary-iOS/Todoary/User/Profile/ProfileImgDataManager/ProfileImgDataManager.swift
@@ -11,20 +11,20 @@ class ProfileImgDataManager {
     
     let headers : HTTPHeaders = [.authorization(UserDefaults.standard.string(forKey: "accessToken")!)]
     
-    let parameters = 
-    
-    let URL = "http://todoary.com/users/profile-img"
-    
-    func profileImgDelete(_ veiwController : ProfileViewController) {
-        AF.upload(multipartFormData: {multipartFormData in
-            for (key, value) in parameters {
-                            multipartFormData.append("\(value)".data(using: .utf8)!, withName: key)
-                        }
-                        if let image = imageData?.pngData() {
-                            multipartFormData.append(image, withName: "activityImage", fileName: "\(image).png", mimeType: "image/png")
-                        }
-                    }, to: URL, usingThreshold: UInt64.init(), method: .patch, headers: headers).response { response in
-                    }
-        }
-    }
-}
+//    let parameters = Parameters
+//    
+//    let URL = "http://todoary.com/users/profile-img"
+//    
+//    func profileImgDelete(_ veiwController : ProfileViewController) {
+//        AF.upload(multipartFormData: {multipartFormData in
+//            for (key, value) in parameters {
+//                            multipartFormData.append("\(value)".data(using: .utf8)!, withName: key)
+//                        }
+//                        if let image = imageData?.pngData() {
+//                            multipartFormData.append(image, withName: "activityImage", fileName: "\(image).png", mimeType: "image/png")
+//                        }
+//                    }, to: URL, usingThreshold: UInt64.init(), method: .patch, headers: headers).response { response in
+//                    }
+//        }
+//    }
+//}
