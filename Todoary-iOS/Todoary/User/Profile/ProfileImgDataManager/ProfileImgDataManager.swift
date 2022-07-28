8986af26e98495457ebfaff9a6c1e92613f20b61
yooyeri <dbdpfl0105@naver.com>
2022-07-28 21:38:41 +0900

    [feat] :  ProfileImg_api 연결

--- Todoary-iOS/Todoary/User/Profile/ProfileImgDataManager/ProfileImgDataManager.swift
+++ Todoary-iOS/Todoary/User/Profile/ProfileImgDataManager/ProfileImgDataManager.swift
@@ -5,4 +5,26 @@
 //  Created by 예리 on 2022/07/22.
 //
 
-import Foundation
+import Alamofire
+
+class ProfileImgDataManager {
+    
+    let headers : HTTPHeaders = [.authorization(UserDefaults.standard.string(forKey: "accessToken")!)]
+    
+    let parameters = 
+    
+    let URL = "http://todoary.com/users/profile-img"
+    
+    func profileImgDelete(_ veiwController : ProfileViewController) {
+        AF.upload(multipartFormData: {multipartFormData in
+            for (key, value) in parameters {
+                            multipartFormData.append("\(value)".data(using: .utf8)!, withName: key)
+                        }
+                        if let image = imageData?.pngData() {
+                            multipartFormData.append(image, withName: "activityImage", fileName: "\(image).png", mimeType: "image/png")
+                        }
+                    }, to: URL, usingThreshold: UInt64.init(), method: .patch, headers: headers).response { response in
+                    }
+        }
+    }
+}
--- Todoary-iOS/Todoary/User/Profile/ProfileImgDeleteDataManager/ProfileImgDeleteDataManager.swift
+++ Todoary-iOS/Todoary/User/Profile/ProfileImgDeleteDataManager/ProfileImgDeleteDataManager.swift
@@ -12,17 +12,27 @@ class ProfileImgDeleteDataManager {
     let headers : HTTPHeaders = [.authorization(UserDefaults.standard.string(forKey: "accessToken")!)]
     
     //통신
-    func profileImgDelete(__ veiwController : ProfileViewController,
-                          _ parameter : ProfileImgInput) {
-        AF.request("http://todoary.com/users/profile-img", method: .delete, parameters: parameter, encoder: JSONParameterEncoder.default, headers: headers).validate().responseDecodable(of: ProfileImgDeleteModel.self)
+    func profileImgDelete(_ veiwController : ProfileViewController) {
+        AF.request("http://todoary.com/users/profile-img",
+                   method: .delete,
+                   encoding: URLEncoding(destination: .queryString),
+                   headers: headers).validate().responseDecodable(of: ProfileImgDeleteModel.self)
         {responce in
-//            switch responce.result {
-//            case .success(let result) :
-//                switch result.code {
-//                case 1000 :
-//
+            switch responce.result {
+            case .success(let result) :
+                switch result.code {
+                case 1000 :
+                    print("사진삭제")
+                case 5001 :
+                    let alert = DataBaseErrorAlert()
+                    veiwController.present(alert, animated: true, completion: nil)
+                default:
+                    print(result.message)
+                }
+            case .failure(let error) :
+                print(error.localizedDescription)
+
                 }
             }
         }
-//    }
-//}
+    }
