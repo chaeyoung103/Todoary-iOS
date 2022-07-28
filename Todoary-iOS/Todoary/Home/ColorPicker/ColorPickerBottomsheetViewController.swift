ad51b2749fc5cf7722b7903e374879545a285c45
yooyeri <dbdpfl0105@naver.com>
2022-07-28 17:13:59 +0900

    [fix] : PlaceholderColor

--- Todoary-iOS/Todoary/Home/ColorPicker/ColorPickerBottomsheetViewController.swift
+++ Todoary-iOS/Todoary/Home/ColorPicker/ColorPickerBottomsheetViewController.swift
@@ -45,6 +45,7 @@ class ColorPickerBottomsheetViewController : UIViewController {
         $0.layer.shadowOpacity = 1
         $0.layer.masksToBounds = false
         $0.placeholder = "카테고리 이름을 입력해 주세요"
+        $0.setPlaceholderColor(.black)
         $0.font = UIFont.nbFont(ofSize: 13, weight: .bold)
         $0.addLeftPadding()
         $0.textFieldTypeSetting(type: .tableCell)
--- Todoary-iOS/Todoary/Home/ColorPicker/ColorPickerViewController.swift
+++ Todoary-iOS/Todoary/Home/ColorPicker/ColorPickerViewController.swift
@@ -57,6 +57,7 @@ class ColorPickerViewController : UIViewController {
         configure()
         setupCollectionView()
         
+        //바텀시트 테스트
         let ColorPickerBottomsheetVC = ColorPickerBottomsheetViewController()
         ColorPickerBottomsheetVC.modalPresentationStyle = .overFullScreen
         self.present(ColorPickerBottomsheetVC, animated: false, completion: nil)
