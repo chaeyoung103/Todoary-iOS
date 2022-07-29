058e8ec7ed7e9cc75603ce608f8dba165bb0a107
chaeyoung103 <thdcodud103@naver.com>
2022-07-29 17:24:13 +0900

    [fix]

---
+++ Todoary-iOS/Todoary/Home/ColorPicker/CategoryMakeDataManager/CategoryMakeInput.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryMakeInput.swift
+//  Todoary
+//
+//  Created by 송채영 on 2022/07/29.
+//
+
+import Foundation
---
+++ Todoary-iOS/Todoary/Home/ColorPicker/CategoryMakeDataManager/CategoryMakeModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryMakeModel.swift
+//  Todoary
+//
+//  Created by 송채영 on 2022/07/29.
+//
+
+import Foundation
