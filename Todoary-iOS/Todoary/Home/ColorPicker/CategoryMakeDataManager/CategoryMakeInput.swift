bff318c5f5ffc11ac8683af43a6f3077158970aa
chaeyoung103 <thdcodud103@naver.com>
2022-07-30 23:53:00 +0900

    [feat]: category생성 api

--- Todoary-iOS/Todoary/Home/ColorPicker/CategoryMakeDataManager/CategoryMakeInput.swift
+++ Todoary-iOS/Todoary/Home/ColorPicker/CategoryMakeDataManager/CategoryMakeInput.swift
@@ -5,4 +5,7 @@
 //  Created by 송채영 on 2022/07/29.
 //
 
-import Foundation
+struct CategoryMakeInput : Encodable {
+    var title : String
+    var color : Int
+}
--- Todoary-iOS/Todoary/Home/ColorPicker/CategoryMakeDataManager/CategoryMakeModel.swift
+++ Todoary-iOS/Todoary/Home/ColorPicker/CategoryMakeDataManager/CategoryMakeModel.swift
@@ -5,4 +5,13 @@
 //  Created by 송채영 on 2022/07/29.
 //
 
-import Foundation
+struct CategoryMakeModel : Decodable {
+    var isSuccess : Bool
+    var code : Int
+    var message : String
+    var result :CategoryMakeResult?
+}
+
+struct CategoryMakeResult : Decodable {
+    var categoryId : CLong?
+}
