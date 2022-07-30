c5837ade2e9bdef17dde62455b73e5f1249db1b0
yooyeri <dbdpfl0105@naver.com>
2022-07-30 22:28:57 +0900

    [feat] : add_DiaryTabelVC

--- Todoary-iOS/Todoary/Diary/DiaryViewController.swift
+++ Todoary-iOS/Todoary/Diary/DiaryViewController.swift
@@ -53,6 +53,7 @@ class DiaryViewController : UIViewController {
         diaryText.setTextWithLineHeight(spaing: 20)
         diaryText.textColor = .silver_225
         diaryText.font = UIFont.nbFont(ofSize: 15, weight: .medium)
+        diaryText.sc
         
         diaryText.delegate = self
 
