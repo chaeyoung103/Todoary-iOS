c72f3dfbb21183578400917a4287090a913c2819
yooyeri <dbdpfl0105@naver.com>
2022-07-30 15:52:50 +0900

    [feat] : diaryText

--- Todoary-iOS/Todoary/Diary/DiaryViewController+Layout.swift
+++ Todoary-iOS/Todoary/Diary/DiaryViewController+Layout.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import SnapKit
+
+
+extension DiaryViewController {
+    
+    func setUpView(){
+        
+        self.view.addSubview()
+
+    }
+    
+    
+    func setUpConstraint(){
+        
+//        //logo
+//        logo.snp.makeConstraints{ make in
+//            make.top.equalToSuperview().offset(56)
+//            make.leading.equalToSuperview().offset(42)
+//            make.width.equalTo(56)
+//            make.height.equalTo(19.27)
+//        }
+    }
+
+}
--- Todoary-iOS/Todoary/Diary/DiaryViewController.swift
+++ Todoary-iOS/Todoary/Diary/DiaryViewController.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+import Then
+
+
+class DiaryViewController : UIViewController {
+    
+    //MARK: - UIComponenets
+    
+    //오늘 날짜
+    let todaysDate = UILabel().then{
+        //선택한 날짜 나오게 연결해주기
+        $0.text = "2022.07.22"
+        $0.font = UIFont.nbFont(ofSize: 16, weight: .bold)
+        $0.addLetterSpacing(spacing: 0.32)
+        $0.textColor = .black
+    }
+    
+    //다이어리 제목
+    let diaryTitle = UITextField().then{
+        $0.placeholder = "오늘의 일기"
+        $0.font = UIFont.nbFont(ofSize: 18, weight: .bold)
+        $0.setPlaceholderColor(.black)
+        $0.addLeftPadding(padding: 0)
+        $0.borderStyle = .none
+    }
+    
+    //다이어리 내용
+    let diaryText = UITextView().then{
+        
+    }
+    
+    let diaryLine =  UIView().then{
+        $0.backgroundColor = .silver_225
+    }
+    
+    
+    //MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        
+//        navigationView = NavigationView(frame: .zero , self.navigationController!)
+        
+        setUpView()
+        setUpConstraint()
+    }
+}
+
+        //MARK: - Helpers
