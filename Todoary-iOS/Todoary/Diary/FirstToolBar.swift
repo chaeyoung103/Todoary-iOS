e2b300b6d0eaf73c209dd0228f86821319fdf1ff
yooyeri <dbdpfl0105@naver.com>
2022-08-01 16:45:33 +0900

    [feat] ; firstToolBar

--- Todoary-iOS/Todoary/Diary/FirstToolBar.swift
+++ Todoary-iOS/Todoary/Diary/FirstToolBar.swift
@@ -10,35 +10,4 @@ import SnapKit
 import Then
 
 class FirstToolBar: UIToolbar {
-    
-    
-    //MARK: - UIComponenets
-    
-//    let cameraBtn = UIButton(_ Type: cus).then{
-//        $0.setImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
-//    }
-//
-//    let textBtn = UIButton().then{
-//        $0.setImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
-//    }
-//
-//    let stickerBtn = UIButton().then{
-//        $0.setImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
-//    }
-//
-//    let highlightBtn = UIButton().then{
-//        $0.setImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
-//    }
-//
-//    let exitBtn = UIButton().then{
-//        $0.setImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
-//    }
-//
-//    let barCameraBtn = UIBarButtonItem.init(customView: cameraBtn)
-//    let barTextBtn = UIBarButtonItem(customView: textBtn)
-//    let barStickerBtn = UIBarButtonItem(customView: stickerBtn)
-//    let barHighlightBtn = UIBarButtonItem(customView: highlightBtn)
-//    let barExitBtn = UIBarButtonItem(customView: exitBtn)
-//
-//    toolbar.items = []
 }
