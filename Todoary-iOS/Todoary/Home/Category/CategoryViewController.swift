53d1c1b580b32c905b348ea7b84eee6e164dc2d3
Ssooya <bsy24660@naver.com>
2022-08-04 18:11:32 +0900

    [refactor]:CategoryVC while문 -> for문으로 변경

--- Todoary-iOS/Todoary/Home/Category/CategoryViewController.swift
+++ Todoary-iOS/Todoary/Home/Category/CategoryViewController.swift
@@ -78,22 +78,19 @@ class CategoryViewController: UIViewController {
     
     @objc
     func trashButtonDidClicked(){
-        
-        print("눌렸니?")
-        
+
         let leading = isEditingMode ? 32 : 58
         let trailing = isEditingMode ? -30 : -4
         
-        var i = 0
-        while(i < todoData.count){
+        for i in 0..<todoData.count{
             guard let cell = tableView.cellForRow(at: [0,i]) as? CategoryTodoTableViewCell else { fatalError() }
             
             cell.contentView.snp.updateConstraints{ make in
                 make.leading.equalToSuperview().offset(leading)
                 make.trailing.equalToSuperview().offset(trailing)
             }
+            
             cell.deleteButton.isHidden.toggle()
-            i = i + 1
         }
         
         isEditingMode.toggle()
