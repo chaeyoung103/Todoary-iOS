7c34531f83edbbdd304b0cdb15cd144b8a89771b
yooyeri <dbdpfl0105@naver.com>
2022-08-11 16:12:48 +0900

    [feat] : stickerBtn

--- Todoary-iOS/Todoary/Diary/DiaryStickerCollectionViewCell.swift
+++ Todoary-iOS/Todoary/Diary/DiaryStickerCollectionViewCell.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class DiaryStickerCollectionViewCell: UICollectionViewCell {
+    static let identifier = "DiaryStickerCollectionViewCell"
+    
+    
+    
+    //MARK: - UIComponenets
+    
+   
+    let stickerBtn = UIImageView().then{
+        $0.image
+
+    }
+   
+    
+    //MARK: - Lifecycles
+    
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+    
+        setUpContentView()
+        setUpConstraint()
+    }
+
+    //MARK: - layout
+        func setUpContentView(){
+            
+            contentView.addSubview(stickerBtn)
+        }
+            
+        
+        func setUpConstraint(){
+            
+            stickerBtn.snp.makeConstraints{ make in
+                make.width.height.equalTo(40)
+                make.centerX.centerY.equalToSuperview()
+                
+            }
+        }
+            
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+}
