6889bac4c1d3a5536f6814550269c8e9a9696baa
yooyeri <dbdpfl0105@naver.com>
2022-08-11 15:15:34 +0900

    [feat] : add_DiaryStickerCollectionView

---
+++ Todoary-iOS/Todoary/Diary/DiaryStickerCollectionView.swift
@@ -0,0 +1,8 @@
+//
+//  DiaryStickerCollectionView.swift
+//  Todoary
+//
+//  Created by 예리 on 2022/08/11.
+//
+
+import Foundation
---
+++ Todoary-iOS/Todoary/Diary/DiaryStickerCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  DiaryStickerCollectionViewCell.swift
+//  Todoary
+//
+//  Created by 예리 on 2022/08/11.
+//
+
+import Foundation
