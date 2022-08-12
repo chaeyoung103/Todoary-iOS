3e3c51a40da23824859dcd7cd5873a8f373d4342
Ssooya <bsy24660@naver.com>
2022-08-12 13:25:57 +0900

    [feat]:다이어리 폰트 파일 추가

---
+++ Todoary-iOS/Todoary/CommonSource/UIFont/DiaryFont.swift
@@ -0,0 +1,22 @@
+//
+//  DiaryFont.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/08/12.
+//
+
+import Foundation
+
+/*
+ GangwonEduAllBold
+ GangwonEduAllLight
+ 
+ IBMPlexSans-Text
+ IBMPlexSans-Medium
+ 
+ IMHyemin-Regular
+ IMHyemin-Bold
+ 
+ NotoSerifKR-Regular
+ NotoSerifKR-SemiBold
+ */
