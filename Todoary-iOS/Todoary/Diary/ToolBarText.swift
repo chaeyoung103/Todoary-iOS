862630b3ceb1b13dc6ade7a1124c3b09560b1236
Ssooya <bsy24660@naver.com>
2022-08-20 10:28:13 +0900

    []:for_merge

--- Todoary-iOS/Todoary/Diary/ToolBarText.swift
+++ Todoary-iOS/Todoary/Diary/ToolBarText.swift
@@ -208,38 +208,13 @@ extension DiaryViewController{
     func registerBtnDidClicked(){
         
         let text = NSAttributedString(attributedString: textView.attributedText)
-        
-        print(text.attributedString2Html!)
 
         let input = DiaryInput(title: diaryTitle.text!,
-                               content: text.attributedString2Html!) //TODO: - API 테스트
-//                               content: textView.text) //임시 테스트용
+                               content: text.attributedString2Html!)
         
-        DiaryDataManager().posts(viewController: self, createdDate: self.sendApiDate, parameter: input)
+        DiaryDataManager().posts(viewController: self, createdDate: self.pickDate!.dateSendServer, parameter: input)
     }
-    
-    /*
-    //NSAttributedString -> html
-    @objc
-    func convertNSAttributeToHtml(){
-        let text = NSAttributedString(attributedString: textView.attributedText)
-        print(text.attributedString2Html)
-    }*/
-    
-    //html -> NSAttributedString
-    
-    func convertHtmlToNSAttributeString(){
-        
-        let text = Optional("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n<html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n<meta http-equiv=\"Content-Style-Type\" content=\"text/css\">\n<title></title>\n<meta name=\"Generator\" content=\"Cocoa HTML Writer\">\n<style type=\"text/css\">\np.p1 {margin: 0.0px 0.0px 0.0px 0.0px; line-height: 25.0px; font: 15.0px \'Apple SD Gothic Neo\'; color: #000000}\nspan.s1 {font-family: \'AppleSDGothicNeo-SemiBold\'; font-weight: bold; font-style: normal; font-size: 15.00px; text-decoration: underline}\nspan.s2 {font-family: \'AppleSDGothicNeo-Medium\'; font-weight: normal; font-style: normal; font-size: 15.00px; text-decoration: underline}\nspan.s3 {font-family: \'AppleSDGothicNeo-Medium\'; font-weight: normal; font-style: normal; font-size: 15.00px; text-decoration: line-through}\n</style>\n</head>\n<body>\n<p class=\"p1\"><span class=\"s1\">&#xAC00;&#xB098;</span><span class=\"s2\">&#xB2E4;&#xB77C;</span><span class=\"s3\">&#xB9C8;&#xBC14;&#xC0AC;&#xC544;</span></p>\n</body>\n</html>\n")
-        
-        if let string = text{
-            let data = Data(string.utf8)
-            
-            if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-                textView.attributedText = attributedString
-            }
-        }
-    }
+
 }
 
 extension UITextInput{
