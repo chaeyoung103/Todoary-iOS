fa4c39833ca4e429a5a24c276fb0cc06d7f10594
Ssooya <bsy24660@naver.com>
2022-08-22 15:29:47 +0900

    []:주석 코드 제거

--- Todoary-iOS/Todoary/API/Auth/LoginDataManager/AppleLoginDataManager.swift
+++ Todoary-iOS/Todoary/API/Auth/LoginDataManager/AppleLoginDataManager.swift
@@ -33,44 +33,5 @@ class AppleLoginDataManager{
                 }
             }
         }
-    
-//        func post(_ viewController : LoginViewController, _ parameter: AppleLoginInput){
-//            AF.request("https://todoary.com/auth/signup/oauth2", method: .post, parameters: parameter, encoder: JSONParameterEncoder.default, headers: nil, interceptor: Interceptor()).validate().responseDecodable(of: ApiModel.self) { response in
-//                switch response.result {
-//                case .success(let result):
-//                    switch result.code{
-//                    case 1000:
-//                        UserDefaults.standard.set(result.result?.token?.accessToken, forKey: "accessToken")
-//                        print("로그인 성공")
-//                        if UserDefaults.standard.bool(forKey: "appPasswordCheck") == true {
-//                            let appPasswordViewController = AppPasswordViewController()
-//                            viewController.navigationController?.pushViewController(appPasswordViewController, animated: true)
-//                            viewController.navigationController?.isNavigationBarHidden = true
-//                        }else {
-//                            let homeViewController = HomeViewController()
-//                            viewController.navigationController?.pushViewController(homeViewController, animated: true)
-//                            viewController.navigationController?.isNavigationBarHidden = true
-//                        }
-//
-//                    case 2005:
-//                        print(result.message)
-//                    case 2011:
-//                        let alert = UIAlertController(title: "회원정보가 존재하지 않습니다.", message: nil, preferredStyle: .alert)
-//                        let action = UIAlertAction(title: "확인", style: .default, handler: nil)
-//                        alert.addAction(action)
-//                        viewController.present(alert, animated: true, completion: nil)
-//                    case 2112:
-//                        let alert = UIAlertController(title: "로그인 정보가 일치하지 않습니다.", message: nil, preferredStyle: .alert)
-//                        let action = UIAlertAction(title: "확인", style: .default, handler: nil)
-//                        alert.addAction(action)
-//                        viewController.present(alert, animated: true, completion: nil)
-//                    default:
-//                        let alert = DataBaseErrorAlert()
-//                        viewController.present(alert, animated: true, completion: nil)
-//                    }
-//                case .failure(let error):
-//                    print(error.localizedDescription)
-//                }
-//            }
 
 }
