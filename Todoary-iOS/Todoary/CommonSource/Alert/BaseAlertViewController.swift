f93e991166e53bfa5de330cd73b8a86ba9c12df5
Ssooya <bsy24660@naver.com>
2022-09-20 10:32:53 +0900

    #293: AlertVC 커스텀 파일 추가

---
+++ Todoary-iOS/Todoary/CommonSource/Alert/BaseAlertViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BaseAlertViewController.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/09/20.
+//
+
+import UIKit
+
+class BaseAlertViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Todoary-iOS/Todoary/CommonSource/Alert/CancelAlertViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CancelAlertViewController.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/09/20.
+//
+
+import UIKit
+
+class CancelAlertViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
