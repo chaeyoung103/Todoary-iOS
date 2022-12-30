292dcf4ce13c34b53829d485c04a1c59c88d50d3
Ssooya <bsy24660@naver.com>
2022-12-30 16:07:09 +0900

    #338: DiaryTest ViewController, View 파일 생성

---
+++ Todoary-iOS/Todoary/Presentation/Views/Diary/DiaryTest/DiaryStickerTestView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryStickerTestView.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/12/30.
+//
+
+import UIKit
+
+class DiaryStickerTestView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Todoary-iOS/Todoary/Presentation/Views/Diary/DiaryTest/DiaryTestView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryTestView.swift
+//  Todoary
+//
+//  Created by 박지윤 on 2022/12/30.
+//
+
+import UIKit
+
+class DiaryTestView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
