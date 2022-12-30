d97fed2a541811655249f3e58b0105c03a4a8b3e
Ssooya <bsy24660@naver.com>
2022-12-30 14:05:41 +0900

    #336: Ask ViewController-View 코드 분리

--- Todoary-iOS/Todoary/Presentation/Views/Setting/AskView.swift
+++ Todoary-iOS/Todoary/Presentation/Views/Setting/AskView.swift
@@ -7,17 +7,35 @@
 
 import Foundation
 
-extension AskViewController{
+class AskView: UIView, BaseViewProtocol{
     
-    func setUpView(){
+    let tableView = UITableView().then{
+        $0.isScrollEnabled = false
         
-        self.view.addSubview(tableView)
+        $0.register(DefaultTableViewCell.self, forCellReuseIdentifier: "askSettingCell")
     }
     
-    func setUpConstraint(){
-        
+    let emailHiddenButton = UIButton().then{
+        $0.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        hierarchy()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func hierarchy(){
+        self.addSubview(tableView)
+    }
+    
+    func layout(){
         tableView.snp.makeConstraints{ make in
-            make.top.equalToSuperview().offset(122)
+            make.top.equalToSuperview().offset(26)
             make.leading.trailing.bottom.equalToSuperview()
         }
     }
